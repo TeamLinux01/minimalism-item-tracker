ca44358072038c59cc48ec26bd1864e296511d06
TeamLinux01 <43735175+TeamLinux01@users.noreply.github.com>
2025-05-02 23:05:23 -0400

    Combobox selection fills in Item data when selected.

--- tracker-GUI.py
+++ tracker-GUI.py
@@ -16,7 +16,8 @@ class MainFrame(ttk.Frame):
         self._itemAmount = 0
         self._itemPrice = 0
 
-        self._items = []
+        self._items = [Item("Computer", "Office", 1, 2000), Item("Spoon", "Kitchen", 10, 2), Item("TV", "Livingroom", 1, 1000)]
+        self.RefreshTextbox()
 
         self.initComponents()
 
@@ -29,6 +30,12 @@ class MainFrame(ttk.Frame):
         self.textFrame.grid(
             column=0, row=1
         )
+
+    def RefreshTextbox(self):
+        self.textFrame.itemsText.configure(state='normal')
+        self.textFrame.itemsText.delete('1.0', 'end')
+        self.textFrame.itemsText.insert('end', self._items)
+        self.textFrame.itemsText.configure(state='disabled')
 
 class CommandFrame(ttk.Frame):
     def __init__(self, parent):
@@ -56,10 +63,10 @@ class CommandFrame(ttk.Frame):
         self.addWindow = AddWindow(self.mainframe, master=root).resizable(0, 0)
 
     def EditItem(self):
-        self.editWindow = EditWindow(master=root).resizable(0, 0)
+        self.editWindow = EditWindow(self.mainframe, master=root).resizable(0, 0)
     
     def DeleteItem(self):
-        self.deleteWindow = DeleteWindow(master=root).resizable(0, 0)
+        self.deleteWindow = DeleteWindow(self.mainframe, master=root).resizable(0, 0)
 
 class TextFrame(ttk.Frame):
     def __init__(self, parent):
@@ -75,11 +82,8 @@ class TextFrame(ttk.Frame):
         self.itemsText.pack(side=tk.LEFT)
 
     def UpdateText(self):
-        self.parent._items.append(Item(self.parent._itemName, self.parent._itemLocation, self.parent._itemAmount, self.parent._itemPrice))
-        self.itemsText.configure(state='normal')
-        self.itemsText.delete('1.0', 'end')
-        self.itemsText.insert('end', self.parent._items)
-        self.itemsText.configure(state='disabled')
+        self.mainframe._items.append(Item(self.mainframe._itemName, self.mainframe._itemLocation, self.mainframe._itemAmount, self.mainframe._itemPrice))
+        self.mainframe.RefreshTextbox()
 
 class AddWindow(Toplevel):
     def __init__(self, parent, master=None):
@@ -110,10 +114,10 @@ class AddItemFrame(ttk.Frame):
         vcmd = (self.register(self.validate),
                 '%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W')
 
-        self.nameLabel = displayLabel(self, text="Name:")
-        self.locationLabel = displayLabel(self, text="Location:")
-        self.amountLabel = displayLabel(self, text="Amount:")
-        self.priceLabel = displayLabel(self, text="Price:")
+        self.labelName = displayLabel(self, text="Name:")
+        self.labelLocation = displayLabel(self, text="Location:")
+        self.labelAmount = displayLabel(self, text="Amount:")
+        self.labelPrice = displayLabel(self, text="Price:")
 
         self.entryName = displayEntry(self, textvariable=self._itemName)
         self.entryLocation = displayEntry(self, textvariable=self._itemLocation)
@@ -127,28 +131,28 @@ class AddItemFrame(ttk.Frame):
         _entryGrid = {"padx":2, "pady":2}
         _buttonGrid = {"padx":2, "pady":2}
 
-        self.nameLabel.grid(
+        self.labelName.grid(
             column=0, row=0, **_labelGrid
         )
         self.entryName.grid(
             column=1, row=0, **_entryGrid
         )
 
-        self.locationLabel.grid(
+        self.labelLocation.grid(
             column=0, row=1, **_labelGrid
         )
         self.entryLocation.grid(
             column=1, row=1, **_entryGrid
         )
 
-        self.amountLabel.grid(
+        self.labelAmount.grid(
             column=2, row=0, **_labelGrid
         )
         self.entryAmount.grid(
             column=3, row=0, **_entryGrid
         )
 
-        self.priceLabel.grid(
+        self.labelPrice.grid(
             column=2, row=1, **_labelGrid
         )
         self.entryPrice.grid(
@@ -182,11 +186,14 @@ class AddItemFrame(ttk.Frame):
         self.mainframe._itemAmount = self._itemAmount.get()
         self.mainframe._itemPrice = self._itemPrice.get()
         self.mainframe.textFrame.UpdateText()
+        self.parent.destroy()
 
 class EditWindow(Toplevel):
-    def __init__(self, master=None):
+    def __init__(self, parent, master=None):
         Toplevel.__init__(self, master)
         self.title("Edit Item")
+        self.parent = parent
+        self.mainframe = parent
 
         self.editItemFrame = EditItemFrame(self)
 
@@ -194,11 +201,14 @@ class EditItemFrame(ttk.Frame):
     def __init__(self, parent):
         ttk.Frame.__init__(self, parent, borderwidth=5, relief="groove", padding="5 5 5 5")
         self.parent = parent
+        self.mainframe = parent.mainframe
+        self._items = self.mainframe._items
+
 
         self._itemIndex = tk.IntVar()
         self._itemName = tk.StringVar()
         self._itemLocation = tk.StringVar()
-        self._itemAmount = tk.IntVar(value=1)
+        self._itemAmount = tk.IntVar()
         self._itemPrice = tk.IntVar()
 
         self.initComponents()
@@ -209,21 +219,26 @@ class EditItemFrame(ttk.Frame):
         vcmd = (self.register(self.validate),
                 '%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W')
 
-        self.indexLabel = displayLabel(self, text="Item Index:")
-        self.nameLabel = displayLabel(self, text="Name:")
-        self.locationLabel = displayLabel(self, text="Location:")
-        self.amountLabel = displayLabel(self, text="Amount:")
-        self.priceLabel = displayLabel(self, text="Price:")
+        self.labelIndex = displayLabel(self, text="Item Index:")
+        self.labelName = displayLabel(self, text="Name:")
+        self.labelLocation = displayLabel(self, text="Location:")
+        self.labelAmount = displayLabel(self, text="Amount:")
+        self.labelPrice = displayLabel(self, text="Price:")
 
-        self.indexCombobox = displayCombobox(self, textvariable=self._itemIndex)
+        self.comboboxIndex = displayCombobox(self, textvariable=self._itemIndex)
 
         self.entryName = displayEntry(self, textvariable=self._itemName)
         self.entryLocation = displayEntry(self, textvariable=self._itemLocation)
         self.entryAmount = displayEntry(self, textvariable=self._itemAmount, validate='key', validatecommand=vcmd)
         self.entryPrice = displayEntry(self, textvariable=self._itemPrice, validate='key', validatecommand=vcmd)
 
-        self.indexCombobox['values'] = ('1', '2', '3')
-        self.indexCombobox['state'] = 'readonly'
+        self.comboboxIndex['values'] = ('1', '2', '3')
+        self.comboboxIndex['state'] = 'readonly'
+
+        def callback(*args):
+            self.RefreshData(int(self.comboboxIndex.get())-1)
+
+        self.comboboxIndex.bind("<<ComboboxSelected>>", callback)
 
         self.cancelButton = displayButton(self, text="Cancel", command=self.Cancel)
         self.enterButton = displayButton(self, text="Enter")
@@ -232,35 +247,35 @@ class EditItemFrame(ttk.Frame):
         _entryGrid = {"padx":2, "pady":2}
         _buttonGrid = {"padx":2, "pady":2}
 
-        self.indexLabel.grid(
+        self.labelIndex.grid(
             column=0, row=0, columnspan=2, **_labelGrid
         )
-        self.indexCombobox.grid(
+        self.comboboxIndex.grid(
             column=2, row=0, columnspan=2, **_entryGrid
         )
 
-        self.nameLabel.grid(
+        self.labelName.grid(
             column=0, row=1, **_labelGrid
         )
         self.entryName.grid(
             column=1, row=1, **_entryGrid
         )
 
-        self.locationLabel.grid(
+        self.labelLocation.grid(
             column=0, row=2, **_labelGrid
         )
         self.entryLocation.grid(
             column=1, row=2, **_entryGrid
         )
 
-        self.amountLabel.grid(
+        self.labelAmount.grid(
             column=2, row=1, **_labelGrid
         )
         self.entryAmount.grid(
             column=3, row=1, **_entryGrid
         )
 
-        self.priceLabel.grid(
+        self.labelPrice.grid(
             column=2, row=2, **_labelGrid
         )
         self.entryPrice.grid(
@@ -274,6 +289,21 @@ class EditItemFrame(ttk.Frame):
             column=3, row=3, **_buttonGrid
         )
 
+        self.RefreshData(0)
+
+    def RefreshData(self, index):
+        i = index
+        self._itemIndex = i+1
+        self.comboboxIndex.set(self._itemIndex)
+        self._itemName = self._items[i].name
+        self.entryName.set_text(self._itemName)
+        self._itemLocation = self._items[i].location
+        self.entryLocation.set_text(self._itemLocation)
+        self._itemAmount = self._items[i].amount
+        self.entryAmount.set_text(self._itemAmount)
+        self._itemPrice = self._items[i].price
+        self.entryPrice.set_text(self._itemPrice)
+
     def validate(self, action, index, value_if_allowed,
                        prior_value, text, validation_type, trigger_type, widget_name):
         if value_if_allowed:
@@ -282,6 +312,8 @@ class EditItemFrame(ttk.Frame):
                 return True
             except ValueError:
                 return False
+        elif value_if_allowed is "":
+            return True
         else:
             return False
 
@@ -289,9 +321,11 @@ class EditItemFrame(ttk.Frame):
         self.parent.destroy()
 
 class DeleteWindow(Toplevel):
-    def __init__(self, master=None):
+    def __init__(self, parent, master=None):
         Toplevel.__init__(self, master)
         self.title("Delete Item")
+        self.parent = parent
+        self.mainframe = parent
 
         self.deleteItemFrame = DeleteItemFrame(self)
 
@@ -299,6 +333,8 @@ class DeleteItemFrame(ttk.Frame):
     def __init__(self, parent):
         ttk.Frame.__init__(self, parent, borderwidth=5, relief="groove", padding="5 5 5 5")
         self.parent = parent
+        self.mainframe = parent.mainframe
+        self._items = self.mainframe._items
 
         self._itemIndex = tk.IntVar()
         self._itemName = tk.StringVar()
@@ -311,25 +347,30 @@ class DeleteItemFrame(ttk.Frame):
     def initComponents(self):
         self.pack(fill=tk.BOTH, expand=True)
 
-        self.indexLabel = displayLabel(self, text="Item Index:")
-        self.nameLabel = displayLabel(self, text="Name:")
-        self.locationLabel = displayLabel(self, text="Location:")
-        self.amountLabel = displayLabel(self, text="Amount:")
-        self.priceLabel = displayLabel(self, text="Price:")
+        self.labelIndex = displayLabel(self, text="Item Index:")
+        self.labelName = displayLabel(self, text="Name:")
+        self.labelLocation = displayLabel(self, text="Location:")
+        self.labelAmount = displayLabel(self, text="Amount:")
+        self.labelPrice = displayLabel(self, text="Price:")
 
-        self.indexCombobox = displayCombobox(self, textvariable=self._itemIndex)
+        self.comboboxIndex = displayCombobox(self, textvariable=self._itemIndex)
 
         self.entryName = displayEntry(self, textvariable=self._itemName)
         self.entryLocation = displayEntry(self, textvariable=self._itemLocation)
         self.entryAmount = displayEntry(self, textvariable=self._itemAmount)
         self.entryPrice = displayEntry(self, textvariable=self._itemPrice)
-        _entries = [self.entryName, self.entryLocation, self.entryAmount, self.entryPrice]
-        for _entry in _entries:
+        self._entries = [self.entryName, self.entryLocation, self.entryAmount, self.entryPrice]
+        for _entry in self._entries:
             _entry['state'] = 'readonly'
 
-        self.indexCombobox['values'] = ('1', '2', '3')
-        self.indexCombobox['state'] = 'readonly'
+        self.comboboxIndex['values'] = ('1', '2', '3')
+        self.comboboxIndex['state'] = 'readonly'
 
+        def callback(*args):
+            self.RefreshData(int(self.comboboxIndex.get())-1)
+
+        self.comboboxIndex.bind("<<ComboboxSelected>>", callback)
+        
         self.cancelButton = displayButton(self, text="Cancel", command=self.Cancel)
         self.enterButton = displayButton(self, text="Enter")
 
@@ -337,35 +378,35 @@ class DeleteItemFrame(ttk.Frame):
         _entryGrid = {"padx":2, "pady":2}
         _buttonGrid = {"padx":2, "pady":2}
 
-        self.indexLabel.grid(
+        self.labelIndex.grid(
             column=0, row=0, columnspan=2, **_labelGrid
         )
-        self.indexCombobox.grid(
+        self.comboboxIndex.grid(
             column=2, row=0, columnspan=2, **_entryGrid
         )
 
-        self.nameLabel.grid(
+        self.labelName.grid(
             column=0, row=1, **_labelGrid
         )
         self.entryName.grid(
             column=1, row=1, **_entryGrid
         )
 
-        self.locationLabel.grid(
+        self.labelLocation.grid(
             column=0, row=2, **_labelGrid
         )
         self.entryLocation.grid(
             column=1, row=2, **_entryGrid
         )
 
-        self.amountLabel.grid(
+        self.labelAmount.grid(
             column=2, row=1, **_labelGrid
         )
         self.entryAmount.grid(
             column=3, row=1, **_entryGrid
         )
 
-        self.priceLabel.grid(
+        self.labelPrice.grid(
             column=2, row=2, **_labelGrid
         )
         self.entryPrice.grid(
@@ -378,6 +419,27 @@ class DeleteItemFrame(ttk.Frame):
         self.enterButton.grid(
             column=3, row=3, **_buttonGrid
         )
+
+        self.RefreshData(0)
+
+    def RefreshData(self, index):
+        for _entry in self._entries:
+            _entry['state'] = 'normal'
+
+        i = index
+        self._itemIndex = i+1
+        self.comboboxIndex.set(self._itemIndex)
+        self._itemName = self._items[i].name
+        self.entryName.set_text(self._itemName)
+        self._itemLocation = self._items[i].location
+        self.entryLocation.set_text(self._itemLocation)
+        self._itemAmount = self._items[i].amount
+        self.entryAmount.set_text(self._itemAmount)
+        self._itemPrice = self._items[i].price
+        self.entryPrice.set_text(self._itemPrice)
+
+        for _entry in self._entries:
+            _entry['state'] = 'readonly'
 
     def Cancel(self):
         self.parent.destroy()
@@ -391,6 +453,10 @@ class displayEntry(ttk.Entry):
     def __init__(self, parent, width=40, **kw):
         ttk.Entry.__init__(self, parent, width=width, **kw)
         self.parent = parent
+
+    def set_text(self, text):
+        self.delete(0,'end')
+        self.insert(0,text)
 
 class displayCombobox(ttk.Combobox):
     def __init__(self, parent, width=40, **kw):
