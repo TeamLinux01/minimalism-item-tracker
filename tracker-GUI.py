e4f189ac300293d9b58bf2de7720f98e819b8e02
TeamLinux01 <43735175+TeamLinux01@users.noreply.github.com>
2025-05-02 21:40:21 -0400

    Working new GUI interface, added classes with defaults, passing list arguments

--- tracker-GUI.py
+++ tracker-GUI.py
@@ -9,15 +9,12 @@ class MainFrame(ttk.Frame):
         ttk.Frame.__init__(self, parent, padding="2 2 2 2")
         self.parent = parent
         self.textFrame = TextFrame(self)
-        self.itemFrame = AddItemFrame(self)
+        self.commandFrame = CommandFrame(self)
 
         self._itemName = ""
         self._itemLocation = ""
         self._itemAmount = 0
         self._itemPrice = 0
-
-        global _rowNumber 
-        _rowNumber = 0
 
         self._items = []
 
@@ -26,109 +23,49 @@ class MainFrame(ttk.Frame):
     def initComponents(self):
         self.pack(fill=tk.BOTH, expand=True)
 
-        self.itemFrame.grid(
+        self.commandFrame.grid(
             column=0, row=0
         )
-
         self.textFrame.grid(
             column=0, row=1
         )
 
-class AddItemFrame(ttk.Frame):
+class CommandFrame(ttk.Frame):
     def __init__(self, parent):
         ttk.Frame.__init__(self, parent, borderwidth=5, relief="groove", padding="5 5 5 5")
         self.parent = parent
-
-        self._itemName = tk.StringVar()
-        self._itemLocation = tk.StringVar()
-        self._itemAmount = tk.IntVar(value=1)
-        self._itemPrice = tk.IntVar()
-
+        self.mainframe = parent
+   
         self.initComponents()
 
     def initComponents(self):
-        self.pack(fill=tk.BOTH, expand=True)
 
-        vcmd = (self.register(self.validate),
-                '%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W')
-
-        _labelWidth = 21
-        _labelPadx = 2
-        _labelPady = 2
-        _labelAnchor = "e"
-        _labelRelief = tk.FLAT
-        _entryWidth = 40
-        _entryPadx = 2
-        _entryPady = 2
-
-        ttk.Label(self, width=_labelWidth, anchor=_labelAnchor, relief=_labelRelief, text="Name:").grid(
-            column=0, row=0, padx=_labelPadx, pady=_labelPady
+        _gridGrid = {"padx":100, "pady":5}
+        
+        self.addItemButton = displayButton(self, text="Add Item", command=self.AddItem).grid(
+            column=0, row=0, **_gridGrid
         )
-        ttk.Entry(self, width=_entryWidth, textvariable=self._itemName).grid(
-            column=1, row=0, padx=_entryPadx, pady=_entryPady
+        self.editItemButton = displayButton(self, text="Edit Item", command=self.EditItem).grid(
+            column=0, row=1, **_gridGrid
         )
-
-        ttk.Label(self, width=_labelWidth, anchor=_labelAnchor, relief=_labelRelief, text="Location:").grid(
-            column=0, row=1, padx=_labelPadx, pady=_labelPady
+        self.deleteItemButton = displayButton(self, text="Delete Item", command=self.DeleteItem).grid(
+            column=0, row=2, **_gridGrid
         )
-        ttk.Entry(self, width=_entryWidth, textvariable=self._itemLocation).grid(
-            column=1, row=1, padx=_entryPadx, pady=_entryPady
-        )
-
-        ttk.Label(self, width=_labelWidth, anchor=_labelAnchor, relief=_labelRelief, text="Amount:").grid(
-            column=2, row=0, padx=_labelPadx, pady=_labelPady
-        )
-        ttk.Entry(self, width=_entryWidth, textvariable=self._itemAmount, validate='key', validatecommand=vcmd).grid(
-            column=3, row=0, padx=_entryPadx, pady=_entryPady
-        )
-
-        ttk.Label(self, width=_labelWidth, anchor=_labelAnchor, relief=_labelRelief, text="Price:").grid(
-            column=2, row=1, padx=_labelPadx, pady=_labelPady
-        )
-        ttk.Entry(self, width=_entryWidth, textvariable=self._itemPrice, validate='key', validatecommand=vcmd).grid(
-            column=3, row=1, padx=_entryPadx
-        )
-
-        self.makeButtons()
-
-    def validate(self, action, index, value_if_allowed,
-                       prior_value, text, validation_type, trigger_type, widget_name):
-        if value_if_allowed:
-            try:
-                float(value_if_allowed)
-                return True
-            except ValueError:
-                return False
-        else:
-            return False
-
-    def makeButtons(self):
-
-        _buttonWidth = 30
-        _buttonPadding = "5 5 5 5"
-        _gridPadx = 100
-        _gridPady = 5
-        ttk.Button(self, text="Add Item", width=_buttonWidth, padding=_buttonPadding, command=self.AddItem).grid(
-            column=4, row=0, padx=_gridPadx, pady=_gridPady
-        )
-        ttk.Button(self, text="Edit Item", width=_buttonWidth, padding=_buttonPadding).grid(
-            column=4, row=1, padx=_gridPadx, pady=_gridPady
-        )
-        ttk.Button(self, text="Delete Item", width=_buttonWidth, padding=_buttonPadding).grid(
-            column=4, row=2, padx=_gridPadx, pady=_gridPady
-        )
-
+    
     def AddItem(self):
-        self.parent._itemName = self._itemName.get()
-        self.parent._itemLocation = self._itemLocation.get()
-        self.parent._itemAmount = self._itemAmount.get()
-        self.parent._itemPrice = self._itemPrice.get()
-        self.parent.textFrame.UpdateText()
+        self.addWindow = AddWindow(self.mainframe, master=root).resizable(0, 0)
+
+    def EditItem(self):
+        self.editWindow = EditWindow(master=root).resizable(0, 0)
+    
+    def DeleteItem(self):
+        self.deleteWindow = DeleteWindow(master=root).resizable(0, 0)
 
 class TextFrame(ttk.Frame):
     def __init__(self, parent):
         ttk.Frame.__init__(self, parent)
         self.parent = parent
+        self.mainframe = parent
 
         self.itemsText = tk.Text(self, height=20, width=190, state='disabled')
         scrollBar = tk.Scrollbar(self, command=self.itemsText.yview)
@@ -144,8 +81,330 @@ class TextFrame(ttk.Frame):
         self.itemsText.insert('end', self.parent._items)
         self.itemsText.configure(state='disabled')
 
+class AddWindow(Toplevel):
+    def __init__(self, parent, master=None):
+        Toplevel.__init__(self, master)
+        self.parent = parent
+        self.mainframe = parent
+        self.title("Add Item")
+
+        self.addItemFrame = AddItemFrame(self)
+
+class AddItemFrame(ttk.Frame):
+    def __init__(self, parent):
+        ttk.Frame.__init__(self, parent, borderwidth=5, relief="groove", padding="5 5 5 5")
+        self.parent = parent
+        self.mainframe = parent.mainframe
+
+        self._itemIndex = tk.IntVar()
+        self._itemName = tk.StringVar()
+        self._itemLocation = tk.StringVar()
+        self._itemAmount = tk.IntVar(value=1)
+        self._itemPrice = tk.IntVar()
+
+        self.initComponents()
+
+    def initComponents(self):
+        self.pack(fill=tk.BOTH, expand=True)
+
+        vcmd = (self.register(self.validate),
+                '%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W')
+
+        self.nameLabel = displayLabel(self, text="Name:")
+        self.locationLabel = displayLabel(self, text="Location:")
+        self.amountLabel = displayLabel(self, text="Amount:")
+        self.priceLabel = displayLabel(self, text="Price:")
+
+        self.entryName = displayEntry(self, textvariable=self._itemName)
+        self.entryLocation = displayEntry(self, textvariable=self._itemLocation)
+        self.entryAmount = displayEntry(self, textvariable=self._itemAmount, validate='key', validatecommand=vcmd)
+        self.entryPrice = displayEntry(self, textvariable=self._itemPrice, validate='key', validatecommand=vcmd)
+
+        self.cancelButton = displayButton(self, text="Cancel", command=self.Cancel)
+        self.enterButton = displayButton(self, text="Enter", command=self.AddItem)
+
+        _labelGrid = {"padx":2, "pady":2}
+        _entryGrid = {"padx":2, "pady":2}
+        _buttonGrid = {"padx":2, "pady":2}
+
+        self.nameLabel.grid(
+            column=0, row=0, **_labelGrid
+        )
+        self.entryName.grid(
+            column=1, row=0, **_entryGrid
+        )
+
+        self.locationLabel.grid(
+            column=0, row=1, **_labelGrid
+        )
+        self.entryLocation.grid(
+            column=1, row=1, **_entryGrid
+        )
+
+        self.amountLabel.grid(
+            column=2, row=0, **_labelGrid
+        )
+        self.entryAmount.grid(
+            column=3, row=0, **_entryGrid
+        )
+
+        self.priceLabel.grid(
+            column=2, row=1, **_labelGrid
+        )
+        self.entryPrice.grid(
+            column=3, row=1, **_entryGrid
+        )
+
+        self.cancelButton.grid(
+            column=2, row=3, **_buttonGrid
+        )
+        self.enterButton.grid(
+            column=3, row=3, **_buttonGrid
+        )
+
+    def validate(self, action, index, value_if_allowed,
+                       prior_value, text, validation_type, trigger_type, widget_name):
+        if value_if_allowed:
+            try:
+                float(value_if_allowed)
+                return True
+            except ValueError:
+                return False
+        else:
+            return False
+
+    def Cancel(self):
+        self.parent.destroy()
+
+    def AddItem(self):
+        self.mainframe._itemName = self._itemName.get()
+        self.mainframe._itemLocation = self._itemLocation.get()
+        self.mainframe._itemAmount = self._itemAmount.get()
+        self.mainframe._itemPrice = self._itemPrice.get()
+        self.mainframe.textFrame.UpdateText()
+
+class EditWindow(Toplevel):
+    def __init__(self, master=None):
+        Toplevel.__init__(self, master)
+        self.title("Edit Item")
+
+        self.editItemFrame = EditItemFrame(self)
+
+class EditItemFrame(ttk.Frame):
+    def __init__(self, parent):
+        ttk.Frame.__init__(self, parent, borderwidth=5, relief="groove", padding="5 5 5 5")
+        self.parent = parent
+
+        self._itemIndex = tk.IntVar()
+        self._itemName = tk.StringVar()
+        self._itemLocation = tk.StringVar()
+        self._itemAmount = tk.IntVar(value=1)
+        self._itemPrice = tk.IntVar()
+
+        self.initComponents()
+
+    def initComponents(self):
+        self.pack(fill=tk.BOTH, expand=True)
+
+        vcmd = (self.register(self.validate),
+                '%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W')
+
+        self.indexLabel = displayLabel(self, text="Item Index:")
+        self.nameLabel = displayLabel(self, text="Name:")
+        self.locationLabel = displayLabel(self, text="Location:")
+        self.amountLabel = displayLabel(self, text="Amount:")
+        self.priceLabel = displayLabel(self, text="Price:")
+
+        self.indexCombobox = displayCombobox(self, textvariable=self._itemIndex)
+
+        self.entryName = displayEntry(self, textvariable=self._itemName)
+        self.entryLocation = displayEntry(self, textvariable=self._itemLocation)
+        self.entryAmount = displayEntry(self, textvariable=self._itemAmount, validate='key', validatecommand=vcmd)
+        self.entryPrice = displayEntry(self, textvariable=self._itemPrice, validate='key', validatecommand=vcmd)
+
+        self.indexCombobox['values'] = ('1', '2', '3')
+        self.indexCombobox['state'] = 'readonly'
+
+        self.cancelButton = displayButton(self, text="Cancel", command=self.Cancel)
+        self.enterButton = displayButton(self, text="Enter")
+
+        _labelGrid = {"padx":2, "pady":2}
+        _entryGrid = {"padx":2, "pady":2}
+        _buttonGrid = {"padx":2, "pady":2}
+
+        self.indexLabel.grid(
+            column=0, row=0, columnspan=2, **_labelGrid
+        )
+        self.indexCombobox.grid(
+            column=2, row=0, columnspan=2, **_entryGrid
+        )
+
+        self.nameLabel.grid(
+            column=0, row=1, **_labelGrid
+        )
+        self.entryName.grid(
+            column=1, row=1, **_entryGrid
+        )
+
+        self.locationLabel.grid(
+            column=0, row=2, **_labelGrid
+        )
+        self.entryLocation.grid(
+            column=1, row=2, **_entryGrid
+        )
+
+        self.amountLabel.grid(
+            column=2, row=1, **_labelGrid
+        )
+        self.entryAmount.grid(
+            column=3, row=1, **_entryGrid
+        )
+
+        self.priceLabel.grid(
+            column=2, row=2, **_labelGrid
+        )
+        self.entryPrice.grid(
+            column=3, row=2, **_entryGrid
+        )
+
+        self.cancelButton.grid(
+            column=2, row=3, **_buttonGrid
+        )
+        self.enterButton.grid(
+            column=3, row=3, **_buttonGrid
+        )
+
+    def validate(self, action, index, value_if_allowed,
+                       prior_value, text, validation_type, trigger_type, widget_name):
+        if value_if_allowed:
+            try:
+                float(value_if_allowed)
+                return True
+            except ValueError:
+                return False
+        else:
+            return False
+
+    def Cancel(self):
+        self.parent.destroy()
+
+class DeleteWindow(Toplevel):
+    def __init__(self, master=None):
+        Toplevel.__init__(self, master)
+        self.title("Delete Item")
+
+        self.deleteItemFrame = DeleteItemFrame(self)
+
+class DeleteItemFrame(ttk.Frame):
+    def __init__(self, parent):
+        ttk.Frame.__init__(self, parent, borderwidth=5, relief="groove", padding="5 5 5 5")
+        self.parent = parent
+
+        self._itemIndex = tk.IntVar()
+        self._itemName = tk.StringVar()
+        self._itemLocation = tk.StringVar()
+        self._itemAmount = tk.IntVar(value=1)
+        self._itemPrice = tk.IntVar()
+
+        self.initComponents()
+
+    def initComponents(self):
+        self.pack(fill=tk.BOTH, expand=True)
+
+        self.indexLabel = displayLabel(self, text="Item Index:")
+        self.nameLabel = displayLabel(self, text="Name:")
+        self.locationLabel = displayLabel(self, text="Location:")
+        self.amountLabel = displayLabel(self, text="Amount:")
+        self.priceLabel = displayLabel(self, text="Price:")
+
+        self.indexCombobox = displayCombobox(self, textvariable=self._itemIndex)
+
+        self.entryName = displayEntry(self, textvariable=self._itemName)
+        self.entryLocation = displayEntry(self, textvariable=self._itemLocation)
+        self.entryAmount = displayEntry(self, textvariable=self._itemAmount)
+        self.entryPrice = displayEntry(self, textvariable=self._itemPrice)
+        _entries = [self.entryName, self.entryLocation, self.entryAmount, self.entryPrice]
+        for _entry in _entries:
+            _entry['state'] = 'readonly'
+
+        self.indexCombobox['values'] = ('1', '2', '3')
+        self.indexCombobox['state'] = 'readonly'
+
+        self.cancelButton = displayButton(self, text="Cancel", command=self.Cancel)
+        self.enterButton = displayButton(self, text="Enter")
+
+        _labelGrid = {"padx":2, "pady":2}
+        _entryGrid = {"padx":2, "pady":2}
+        _buttonGrid = {"padx":2, "pady":2}
+
+        self.indexLabel.grid(
+            column=0, row=0, columnspan=2, **_labelGrid
+        )
+        self.indexCombobox.grid(
+            column=2, row=0, columnspan=2, **_entryGrid
+        )
+
+        self.nameLabel.grid(
+            column=0, row=1, **_labelGrid
+        )
+        self.entryName.grid(
+            column=1, row=1, **_entryGrid
+        )
+
+        self.locationLabel.grid(
+            column=0, row=2, **_labelGrid
+        )
+        self.entryLocation.grid(
+            column=1, row=2, **_entryGrid
+        )
+
+        self.amountLabel.grid(
+            column=2, row=1, **_labelGrid
+        )
+        self.entryAmount.grid(
+            column=3, row=1, **_entryGrid
+        )
+
+        self.priceLabel.grid(
+            column=2, row=2, **_labelGrid
+        )
+        self.entryPrice.grid(
+            column=3, row=2, **_entryGrid
+        )
+
+        self.cancelButton.grid(
+            column=2, row=3, **_buttonGrid
+        )
+        self.enterButton.grid(
+            column=3, row=3, **_buttonGrid
+        )
+
+    def Cancel(self):
+        self.parent.destroy()
+
+class displayLabel(ttk.Label):
+    def __init__(self, parent, width=25, anchor="e", relief=tk.FLAT, **kw):
+        ttk.Label.__init__(self, parent, width=width, anchor=anchor, relief=relief, **kw)
+        self.parent = parent
+
+class displayEntry(ttk.Entry):
+    def __init__(self, parent, width=40, **kw):
+        ttk.Entry.__init__(self, parent, width=width, **kw)
+        self.parent = parent
+
+class displayCombobox(ttk.Combobox):
+    def __init__(self, parent, width=40, **kw):
+        ttk.Combobox.__init__(self, parent, width=width, **kw)
+        self.parent = parent
+
+class displayButton(ttk.Button):
+    def __init__(self, parent, width=30, padding="5 5 5 5", **kw):
+        ttk.Button.__init__(self, parent, width=width, padding=padding, **kw)
+        self.parent = parent
+
 if __name__ == "__main__":
     root = tk.Tk()
+    root.resizable(0, 0)
     root.title("Minimalist Item Tracker")
     MainFrame(root)
     root.mainloop()
